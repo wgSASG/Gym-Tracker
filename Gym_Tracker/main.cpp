05dad996c2c1159edf9aa35fbd1784eee78eba88
arturo2382 <131068812+wgSASG@users.noreply.github.com>
2026-06-13 16:41:05 -0600

    Add files via upload

---
+++ Gym_Tracker/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Ejercicio
+//
+//  Created by Arturo Sebastian García Soto on 09/06/26.
+//
+
+
+
+#include "TrackerApp.hpp"
+
+int main() {
+    TrackerApp miApp;
+    miApp.iniciarMenu();
+    return 0;
+}
